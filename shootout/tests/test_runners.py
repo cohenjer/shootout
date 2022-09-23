22485a88d01aae228033e568d33bc0c30b47b7e6
COHEN Jeremy <jeremy.cohen@cnrs.fr>
2022-09-23 15:22:14 +0200

    fixing runners test

--- shootout/tests/test_runners.py
+++ shootout/tests/test_runners.py
@@ -4,18 +4,18 @@ import os
 
 # Testing run_and_track
 # Not much to test here; just check that a DataFrame is created and that this function runs smoothly
-@run_and_track(n=[10,15], m=[25,30],noise=0.25, add_track={"mytrack":[7,12,20]}, name_store="test-df", nb_seeds=2, algorithm_names=["method1","method2"])
-def myalgorithm(n=10,m=20,r=3,random=True,noise=0.5):
-    w = m+n+r+noise
+@run_and_track(n=[10,15], m=[25,30],noise=0.25, add_track={"mytrack":[7,12,20]}, name_store="test-df", nb_seeds=2, algorithm_names=["method1","method2"], seeded_fun=True)
+def myalgorithm(n=10,m=20,r=3,random=True,noise=0.5, seed=0):
+    w = m+n+r+noise+seed
     h = [1,2,3,4]
-    err = [n-m, n+m]
+    err = [n-m, n+m+seed]
     time = [0,0.2]
     return {"errors":[err,err],"timings": [time,time],"myoutput":[h,h],"scalaroutput":7}
 # We check if the dataframe has been created by loading it
 df = pd.read_pickle("test-df")
 print(df)
 # picking a random element to check
-assert df["errors"][6][0] == -20
+assert df["errors"][6][1] == 41
 # deleting temporary df
 os.remove("test-df")
 
